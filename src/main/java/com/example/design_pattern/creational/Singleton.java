7e80d29b53509d9afee865fec412c4ee036417d4
HackAndBuild <mohanthi205@gmail.com>
2025-11-29 18:39:05 +0530

    design pattern

---
+++ src/main/java/com/example/design_pattern/creational/Singleton.java
@@ -0,0 +1,18 @@
+package com.example.design_pattern.creational;
+
+import lombok.Synchronized;
+
+public class Singleton {
+    private static Singleton instance = null;
+
+    private Singleton() {
+    }
+
+    @Synchronized
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
